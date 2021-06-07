a17067ebc843d9464fa273968650bf14108a60e8
jyoti-wp <sim_s006@icloud.com>
2021-06-08 00:15:50 +0530

    parctical 4 update

--- 4.php
+++ 4.php
@@ -1,36 +1,58 @@
 <?php include "functions.php"; ?>
-<?php include "includes/header.php";?>
+<?php include "includes/header.php"; ?>
 
-	<section class="content">
+<section class="content">
 
 	<aside class="col-xs-4">
 
-		<?php Navigation();?>
-			
-		
-	</aside><!--SIDEBAR-->
+		<?php Navigation(); ?>
 
 
-<article class="main-content col-xs-8">
+	</aside>
+	<!--SIDEBAR-->
 
-	
-	<?php  
 
-/*  Step1: Define a function and make it return a calculation of 2 numbers
+	<article class="main-content col-xs-8">
 
-	Step 2: Make a function that passes parameters and call it using parameter values
+
+		<?php
+
+		/*  Step1: Define a function and make it return a calculation of 2 numbers
+*/
+
+		function calculate($number1, $number2)
+		{
+			$sum = $number1 + $number2;
+			return $sum;
+		}
+
+		$result = calculate(365, 753);
+		echo $result;
+
+		/*Step 2: Make a function that passes parameters and call it using parameter values
 
 
  */
+		function value($value1, $value2)
+		{
+			$sum = $value1 + $value2;
+			return $sum;
+		}
 
-	
-?>
+		$result = value(34, 28);
+		echo "<br>";
+		echo $result . "<br>";
+		$result = value(64, $result);
+		echo $result;
+
+		?>
 
 
 
 
 
-</article><!--MAIN CONTENT-->
+	</article>
+	<!--MAIN CONTENT-->
 
 
-<?php include "includes/footer.php"; ?>
+	<?php include "includes/footer.php"; ?>
