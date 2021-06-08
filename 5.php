0121378876b16faa34d1bfa05accccbbec65745d
jyoti-wp <sim_s006@icloud.com>
2021-06-08 16:44:50 +0530

    practice session 5 update

--- 5.php
+++ 5.php
@@ -1,21 +1,31 @@
 <?php include "functions.php"; ?>
-<?php include "includes/header.php";?>
-	<section class="content">
+<?php include "includes/header.php"; ?>
+<section class="content">
 
-		<aside class="col-xs-4">
-		<?php Navigation();?>
-			
-			
-		</aside><!--SIDEBAR-->
+	<aside class="col-xs-4">
+		<?php Navigation(); ?>
 
 
-<article class="main-content col-xs-8">
-
-	
-	<?php 
+	</aside>
+	<!--SIDEBAR-->
 
 
-/*  Step1: Use a pre-built math function here and echo it
+	<article class="main-content col-xs-8">
+
+
+		<?php
+
+		echo ceil(4.5);
+		echo "<br>";
+		$string = "sdgfkgfkhjvsfkgagiufnaklsdnfcljkshbfuksd";
+		$value = strlen($string);
+		echo $value;
+		echo "<br>";
+
+		$newValue = [3435, 756476, 87967, $string];
+		in_array($string, $newValue);
+		echo in_array($string);
+		/*  Step1: Use a pre-built math function here and echo it
 
 
 	Step 2:  Use a pre-built string function here and echo it
@@ -25,12 +35,13 @@
 
  */
 
-	
-?>
+
+		?>
 
 
 
 
 
-</article><!--MAIN CONTENT-->
-<?php include "includes/footer.php"; ?>
+	</article>
+	<!--MAIN CONTENT-->
+	<?php include "includes/footer.php"; ?>
